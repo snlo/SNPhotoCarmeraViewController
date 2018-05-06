4bf88ee66b6f1ed9252f1860c0d6d2df049df312
MrSunDong <snloveydus@sina.com>
2018-05-07 00:12:24 +0800

    update 0.0.1

---
+++ SNPhotoCarmeraViewControllor/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  SNPhotoCarmeraViewControllor
+//
+//  Created by snlo on 2018/5/6.
+//  Copyright © 2018年 snlo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import "SNPhotoCameraViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
