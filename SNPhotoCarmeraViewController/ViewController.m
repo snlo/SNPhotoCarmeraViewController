b5421f04b0270898c740a411461b983ee182d847
snlo <snloveydus@sina.com>
2018-05-11 18:24:14 +0800

    测试

--- SNPhotoCarmeraViewController/ViewController.m
+++ SNPhotoCarmeraViewController/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+//    pod trunk push SNPhotoCarmeraViewController.podspec --verbose --allow-warnings --use-libraries
 }
 
 
